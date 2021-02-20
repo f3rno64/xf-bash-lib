6752ef5d9854b66e2dff4603f363502a176f025e
Cris Mihalache <xf3rno@pm.me>
2021-02-20 15:26:23 +0700

    fix: rvm init

--- lib/core/rvm.sh
+++ lib/core/rvm.sh
@@ -7,11 +7,5 @@ xf_has_rvm() {
 xf_rvm_init() {
   if ! xf_has_rvm; then return; fi
 
-  export RVM_PATH="$HOME/.rvm"
-  export RUBY_BIN_PATH="$RVM_PATH/rubies/default/bin"
-  export GEM_BIN_PATH="$RVM_PATH/gem/default/bin"
-
-  xf_safe_add_dir_to_path "$RVM_PATH"
-  xf_safe_add_dir_to_path "$GEM_BIN_PATH"
-  xf_safe_add_dir_to_path "$RUBY_BIN_PATH"
+  xf_safe_source "$HOME/.rvm/environments/default" '> /dev/null 2>&1'
 }

06a836e99ee40be3f3b18e423b15cb5f4090b2dc
Mircea Cristian Mihalache <mf3rnol@gmail.com>
2021-10-16 09:58:22 +0000

    fix: update fzf default command env var name

--- lib/core/fzf.sh
+++ lib/core/fzf.sh
@@ -6,7 +6,7 @@ xf_has_fzf() {
 
 # TODO: Use array for list of ignore directories
 xf_fzf_init() {
-  export FZF_CTRL_T_COMMAND='ag --hidden --ignore-dir={.git,.undodir,docs/dist,node_modules,bower_components,dist} -g ""'
+  export FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir={.git,.undodir,docs/dist,node_modules,bower_components,dist} -g ""'
 
   xf_safe_source "$HOME/.fzf.bash"
 }

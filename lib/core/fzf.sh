3b0b4040739bba1d06b7ddec7e69c5e2876c94ae
Mircea Cristian Mihalache <f3rno64@gmail.com>
2022-01-15 15:51:46 +0200

    fix: update ag ignore-dir arg usage

--- lib/core/fzf.sh
+++ lib/core/fzf.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 xf_has_fzf() {
-  xf_has_cmd 'fzf'
+    xf_has_cmd 'fzf'
 }
 
 # TODO: Use array for list of ignore directories
 xf_fzf_init() {
-  export FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir={.git,.undodir,docs/dist,node_modules,bower_components,dist} -g ""'
-
-  xf_safe_source "$HOME/.fzf.bash"
+    export FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir=.git --ignore-dir=.undodir --ignore-dir=docs/dist --ignore-dir=node_modules --ignore-dir=bower_components --ignore-dir=dist -g ""'
+    
+    xf_safe_source "$HOME/.fzf.bash"
 }

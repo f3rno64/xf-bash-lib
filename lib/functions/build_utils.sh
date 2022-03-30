ef4d929acae2fbd2f75021b378a76c8d5c608336
Cris Mihalache <cris.mihalache@tradeshift.com>
2022-03-31 02:27:38 +0300

    fix: git core count resolution on macos

--- lib/functions/build_utils.sh
+++ lib/functions/build_utils.sh
@@ -1,7 +1,17 @@
 #! /usr/bin/env bash
 
-CPU_INFO="$(cat /proc/cpuinfo)"
-CORE_COUNT=$(echo "$CPU_INFO" | grep cores | tail -n1 | awk "{ TS=\" \" } { print $4 }")
+UNAME="$(uname -a)"
+
+if [[ "$UNAME" =~ Darwin ]]; then
+  CPU_INFO="$(sysctl -a machdep.cpu)"
+  RAW_CORE_COUNT=$(echo "$CPU_INFO" | grep core_count)
+  CORE_COUNT=$(echo "$RAW_CORE_COUNT" | awk '{ TS=": " } { print $2 }')
+elif [[ -f "/proc/cpuinfo" ]]; then
+  CPU_INFO="$(cat /proc/cpuinfo)"
+  CORE_COUNT=$(echo "$CPU_INFO" | grep cores | tail -n1 | awk "{ TS=\" \" } { print $4 }")
+else
+  CORE_COUNT=1
+fi
 
 conf() {
   if [[ -x './configure' ]]; then

135fb9c19df41baf99d11400a2f3053201ec7c0e
Cris Mihalache <f3rno64@gmail.com>
2023-05-23 19:53:19 +0300

    fix: go

--- lib/core/go.sh
+++ lib/core/go.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 xf_has_go() {
-  xf_has_dir "$HOME/.go"
+  xf_has_dir "$HOME/go"
 }
 
 xf_go_init() {
   if ! xf_has_go; then return; fi
 
-  export GO_PATH="$HOME/.go"
+  export GO_PATH="$HOME/go"
   export GO_BIN="$GO_PATH/bin"
 
   xf_safe_add_dir_to_path "$GO_BIN"

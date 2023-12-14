c64a9210304ea76c91c4cced0d88d1ded32649d0
Cris Mihalache <f3rno64@gmail.com>
2023-12-14 22:57:59 +0200

    feature: add aliases for npm

---
+++ lib/aliases/npm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+alias n="npm"
+alias ns="npm run start"
+alias nt="npm run test"
+alias nl="npm run lint"
+alias nb="npm run build"
+alias nu="npm run update-version"
+alias no="npm run outdated"
+alias ni="npm run init"

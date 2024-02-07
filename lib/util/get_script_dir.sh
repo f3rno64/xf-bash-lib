57a3db721bb79f378645b7221f728192eb5ee10b
Cris Mihalache <f3rno64@gmail.com>
2024-02-07 23:20:05 +0100

    fix: shfmt issue with get_script_dir.sh

--- lib/util/get_script_dir.sh
+++ lib/util/get_script_dir.sh
@@ -5,8 +5,9 @@
 
 xf_get_script_dir() {
   local -r SOURCE_FN="${1:${BASH_SOURCE[0]}}"
-  local -r THIS_DIR="${2:(dirname -- "$SOURCE_FN")}"
-  local -r SCRIPT_DIR="$(cd -- "$(dirname -- "$THIS_DIR")")"
+  local -r THIS_DIR="$(dirname -- "$SOURCE_FN")"
+  local -r FINAL_DIR="${2:$THIS_DIR}"
+  local -r SCRIPT_DIR="$(cd -- "$(dirname -- "$FINAL_DIR")")"
 
   echo "$(cd -- $SCRIPT_DIR &> /dev/null && pwd)"
 }

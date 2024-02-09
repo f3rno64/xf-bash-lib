4106b02244fa853e6fc27b9db9afdf690ff77a06
Cris Mihalache <f3rno64@gmail.com>
2024-02-09 19:12:40 +0200

    refactor(aliases): rm cp-progress alias

--- lib/aliases/cp.sh
+++ lib/aliases/cp.sh
@@ -9,7 +9,4 @@ fi
 if xf_has_cmd 'pycp'; then
   alias cppg="pycp -i -p -g"
   xf_log_success "aliased cppg as pycp -i -p -g"
-
-  alias cp-progress="pycp -i -p -g"
-  xf_log_success "aliased cp-progress as pycp -i -p -g"
 fi

27659ac7e7c2ba5d825db3a3df9ef6fa3573a555
Cris Mihalache <f3rno64@gmail.com>
2024-02-09 17:34:12 +0200

    feat(aliases): add pycp aliases cppg and cp-progress

--- lib/aliases/cp.sh
+++ lib/aliases/cp.sh
@@ -5,3 +5,11 @@ if xf_has_cmd 'fcp'; then
 
   xf_log_success "aliased cp as fcp"
 fi
+
+if xf_has_cmd 'pycp'; then
+  alias cppg="pycp -i -p -g"
+  xf_log_success "aliased cppg as pycp -i -p -g"
+
+  alias cp-progress="pycp -i -p -g"
+  xf_log_success "aliased cp-progress as pycp -i -p -g"
+fi

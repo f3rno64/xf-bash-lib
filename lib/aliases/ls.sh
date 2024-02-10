656383f263860161cd8f7f45388533fb761744fc
Cris Mihalache <f3rno64@gmail.com>
2024-02-10 04:03:33 +0200

    feat(aliases): add more useful flags to lsd in ls aliasesD

--- lib/aliases/ls.sh
+++ lib/aliases/ls.sh
@@ -7,47 +7,74 @@
 #
 # Adds helpful ls aliases:
 #
-# * ls - base ls command unconfigured
+# * ls - base ls command
 # * ll - list all as vertical list
-# * la - list all as vertical listing
 # * lt - list all as vertical listing, most recent first
 # * lh - list only hidden files (as `la`)
+#
+# Uses lsd if it is available, otherwise uses the builtin ls command.
 
-# {{{ base ls command arguments
+# {{{ base ls and lsd command arguments
 
 LS_HELP="$(ls -h)"
+LSD_HELP="$(lsd -h)"
 
-declare -a LS_CMD_ARGS=(
+declare -a LSD_BASE_CMD_ARGS=(
+  '-h'
+  '-A'
+  '-F'
+  '-L'
+  '--total-size'
+  '--no-symlink'
+  '--color=always'
+  '--date relative'
+)
+
+declare -a LS_BASE_CMD_ARGS=(
   '--color=always'
   '-h'
 )
 
 # }}}
-# {{{ add --group-directories-first if supported
+# {{{ add --group-directories-first arg if supported to LSD_CMD_ARGS
+
+if [[ "$LSD_HELP" =~ '--group-directories-first' ]]; then
+  LSD_CMD_ARGS="${LSD_BASE_CMD_ARGS[*]}"
+  LSD_CMD_ARGS+=('--group-directories-first')
+
+  xf_log_success "lsd will group directories"
+fi
+
+# }}}
+# {{{ add --group-directories-first arg if supported to LS_CMD_ARGS
 
 if [[ "$LS_HELP" =~ '--group-directories-first' ]]; then
+  LS_CMD_ARGS="${LS_BASE_CMD_ARGS[*]}"
   LS_CMD_ARGS+=('--group-directories-first')
 
-  xf_log_success "ls alias will group directories"
+  xf_log_success "ls will group directories"
 fi
 
 # }}}
 # {{{ aliases
 
+LSD_CMD_ARGS_STR="$(IFS=" "; echo "${LSD_CMD_ARGS[*]}")"
 LS_CMD_ARGS_STR="$(IFS=" "; echo "${LS_CMD_ARGS[*]}")"
 
-if command -v lsd >/dev/null 2>&1; then
-  alias ls="lsd $LS_CMD_ARGS_STR"
-  alias ll="lsd -al"
-  alias lt="lsd -alt"
-  alias lh="lsd -alt .*"
+if xf_has_cmd 'lsd'; then
+  alias ls="lsd $LSD_CMD_ARGS_STR"
+  alias ll="lsd $LSD_CMD_ARGS_STR -l"
+  alias lt="lsd $LSD_CMD_ARGS_STR -l -t"
+  alias lh="lsd $LSD_CMD_ARGS_STR -l -t .*"
+
+  xf_log_success 'added ls aliases using lsd'
 else
   alias ls="ls $LS_CMD_ARGS_STR"
-  alias ll="ls -al"
-  alias lt="ls -alt"
-  alias lh="ls -alt .*"
-fi
+  alias ll="ls $LS_CMD_ARGS_STR -l"
+  alias lt="ls $LS_CMD_ARGS_STR -l -t"
+  alias lh="ls $LS_CMD_ARGS_STR -l -t .*"
 
-xf_log_success "added ls aliases"
+  xf_log_success 'added ls aliases'
+fi
 
 # }}}

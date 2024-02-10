5d82a4631ebf968dffd67907240620e1c16a8887
Cris Mihalache <f3rno64@gmail.com>
2024-02-10 02:18:32 +0200

    feat(functions): add more pnpm functions (pna, pnad, pnag, pnu, pnug)

--- lib/functions/pnpm.sh
+++ lib/functions/pnpm.sh
@@ -1,9 +1,44 @@
 #!/usr/bin/env bash
 
-pnad() {
-  pnpm add -D "$1"
+pna() {
+  if [[ -z "$*" ]]; then
+    echo "Usage: pna <package>"
+    return 1
+  fi
+
+  pnpm add "$@"
 }
 
-pna() {
-  pnpm add "$1"
+pnad() {
+  if [[ -z "$*" ]]; then
+    echo "Usage: pnad <package>"
+    return 1
+  fi
+
+  pnpm add -D "$@"
+}
+
+pnag() {
+  if [[ -z "$*" ]]; then
+    echo "Usage: pnag <package>"
+    return 1
+  fi
+
+  pnpm add -g "$@"
+}
+
+pnu() {
+  if [[ -z "$*" ]]; then
+    pnpm update
+  else
+    pnpm update "$@"
+  fi
+}
+
+pnug() {
+  if [[ -z "$*" ]]; then
+    pnpm update -g
+  else
+    pnpm update -g "$@"
+  fi
 }

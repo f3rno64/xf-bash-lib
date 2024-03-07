15c226ec335acb42105275ab427a7dec64fdd002
Cris Mihalache <f3rno64@gmail.com>
2024-03-07 17:46:33 +0200

    fix: gh aliases no longer exist

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -4,7 +4,6 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
 source "$DIR/aliases/cp.sh"
-source "$DIR/aliases/gh.sh"
 source "$DIR/aliases/ls.sh"
 source "$DIR/aliases/git.sh"
 source "$DIR/aliases/cat.sh"
